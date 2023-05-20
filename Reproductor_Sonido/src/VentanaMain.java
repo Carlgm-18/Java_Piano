f3061ebab60d39ecb644e17720a3afe5f4aa590b
Carlos Gálvez <c.galvezmena@gmail.com>
2023-05-20 11:06:32 +0200

    Inicialized

---
+++ Reproductor_Sonido/src/VentanaMain.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+
+public class VentanaMain extends JFrame {
+    public VentanaMain(){
+        this.setSize(1235,1200);
+        this.setTitle("TALLER 2 - PROGRAMACIÓN II - CURSO 2022-2023");
+        this.setContentPane(new PanelContenidos());
+
+    }
+
+    /*private void init(){
+        JPanel dibujo = new JPanel();
+        JPanel panelActividades = new JPanel();
+
+        JPanel panelBotones = new JPanel();
+        JButton crear = new JButton("CREAR");
+        JButton reproducir = new JButton("REPRODUCIR");
+        JButton adivinar = new JButton("ADIVINAR");
+        JButton salir = new JButton("SALIR");
+        panelBotones.ad
+
+        JSplitPane logo =
+        JSplitPane splitUpper
+        JSplitPane splitLower
+        JSplitPane general = new JSplitPane();
+        JPanel panelContenidos = new JPanel();
+    }*/
+/*
+    private void colorcarBotones(String[] names){
+        JButton[] botones = new JButton[names.length];
+        for(int i = 0; i<names.length; i++){
+            botones[i] = new JButton();
+            botones[i].
+        }
+    }*/
+}
